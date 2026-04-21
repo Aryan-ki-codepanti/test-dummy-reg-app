3ced34578670e0a1610aa8e68f857ac78e3f489d
Aryan <71351433+Aryan-ki-codepanti@users.noreply.github.com>
2026-04-21 15:16:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import joblib
 import numpy as np
+import requests
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -10,19 +11,23 @@ model = joblib.load("linear_regression_model.joblib")
 app = FastAPI()
 
 # 2. Define the input schema
-
-
 class RegInput(BaseModel):
     experience: float
 
-# 3. Create the prediction endpoint
-
-
+# 3. Prediction endpoint
 @app.post("/predict")
 def predict(data: RegInput):
-    # Convert Pydantic object to NumPy array
     features = np.array([[data.experience]])
-
-    # Get prediction
     prediction = model.predict(features)
     return {"prediction": prediction[0]}
+
+# 4. Dummy users endpoint
+@app.get("/users")
+def get_users():
+    url = "https://jsonplaceholder.typicode.com/users"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return {"error": "Failed to fetch users"}
+
+    return response.json()
